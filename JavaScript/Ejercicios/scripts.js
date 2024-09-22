ea2441d07753cff47753bd43d7bdb0127f6ae090
David <dricciodeabreu1@gmail.com>
2024-09-22 14:51:34 +0100

    cambios

--- JavaScript/Ejercicios/scripts.js
+++ JavaScript/Ejercicios/scripts.js
@@ -1,36 +1,30 @@
 /* Ejercicio 1 */
 
-function prueba(){
-    let a = document.getElementById("1").value;
-    let b = document.getElementById("2").value;
-    let c = document.getElementById("3").value;
-
-    if (a<0||b<0||c<0){
-        console.error("Error: numero menor a 0")
-    }
-    else if (a==0 && b==0 && c==0) {
-        console.error("Error todos los numeros son 0");
-    }
-    else if ((a + b + c)>10 && a != b && b != c && c != a){
-        console.error("Error el numero es mayor de 10 y los numeros son diferentes")
+function prueba(a, b, c) {
+    if (a < 0 || b < 0 || c < 0) {
+        console.error("Error: número menor a 0");
+    } else if (a == 0 && b == 0 && c == 0) {
+        console.error("Error: todos los números son 0");
+    } else if ((a + b + c) > 10 && a !== b && b !== c && c !== a) {
+        console.error("Error: el número es mayor de 10 y los números son diferentes");
     }
 }
+
+
 
 /* Ejercicio 2 */
- function prueba2(){
-    let a = document.getElementById("4").value;
-    if (a === 0){
-        console.log("‘Este es muy fácil… ¡prueba otro!")
-    } else if (a == 2 || a ==4 || a ==6){
-        console.log("El número es par")
-        
-    }else if (a == 1 || a ==3 || a ==5){
-        console.log("El número es impar")
-    }else{
-        console.log("‘¡¡Sólo sé contar de 0 a 6!!")
+function prueba2(a) {
+    if (a === 0) {
+        console.log("‘Este es muy fácil… ¡prueba otro!");
+    } else if (a == 2 || a == 4 || a == 6) {
+        console.log("El número es par");
+    } else if (a == 1 || a == 3 || a == 5) {
+        console.log("El número es impar");
+    } else {
+        console.log("‘¡¡Sólo sé contar de 0 a 6!!");
     }
-
 }
+
 
 /* Ejercicio 3 */
 function prueba3() {
@@ -42,20 +36,21 @@ function prueba3() {
 
 /* Ejercicio 4 */
 let result = 1, arg1, arg2, res1, res2;
-while (result > 0) {
- arg2 = Math.random();
- res2 = res1/(arg1 + arg2);
- arg1 = Math.random() - 0.5;
- res1 = Math.sqrt(arg1);
- if (res1 < 0){
-    continue
- }else if (arg1 + arg2 ==0){
-    break
- }
 
- result = res1 + res2;
- console.log(result);
+while (result > 0) {
+    arg1 = Math.random() - 0.5;
+    res1 = Math.sqrt(Math.max(arg1, 0));
+    arg2 = Math.random();
+    
+    if (arg1 + arg2 === 0) {
+        break;
+    }
+
+    res2 = res1 / (arg1 + arg2);
+    result = res1 + res2;
+    console.log(result);
 }
+
 
 /* Ejercicio 5 */
 
